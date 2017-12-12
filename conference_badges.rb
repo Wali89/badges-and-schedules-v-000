db83eb1539394a850dc4622f7cc173653076f51a
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-12 13:36:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,3 +16,13 @@ def assign_rooms(attendees)
 end
 
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
