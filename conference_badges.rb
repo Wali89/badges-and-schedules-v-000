15a66134186d9e18cf91a01a8908052827c3e635
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-12 12:53:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
   array.map do |name|
-    puts "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
   end
 end
